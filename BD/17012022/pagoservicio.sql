f4c79e73cdd22239dd1dae43a70c5b9340b74fbc
Nestor Lopez <nestorj.lopez@gmail.com>
2022-01-17 14:58:14 -0600

    DB tabla pago servicio

---
+++ BD/17012022/pagoservicio.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-01-2022 a las 21:56:22
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `qido`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagoservicio`
+--
+
+CREATE TABLE `pagoservicio` (
+  `idPagoServicio` int(11) NOT NULL,
+  `idServicio` int(11) NOT NULL,
+  `monto` double(10,2) NOT NULL,
+  `motivo` varchar(300) NOT NULL,
+  `fechaPago` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
