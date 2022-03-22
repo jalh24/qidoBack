5baf2a2eb2ef041a7f30c73143940d5a61efce79
jalh24 <jalh24@hotmail.com>
2022-03-22 15:07:57 -0600

    estado cuenta

--- app/Controllers/EstadoCuenta.php
+++ app/Controllers/EstadoCuenta.php
@@ -17,11 +17,23 @@ class EstadoCuenta extends BaseController
 	}
 
     
-    public function listaPacientes()
-	{
-		$estadoCuentaModel = new EstadoCuentaModel();
-		$resp["data"]=$estadoCuentaModel->getListaPacientes($this->request->getVar('cliente'));
-		return $this->respond($resp);
-	}
+    // public function listaPacientes()
+	// {
+	// 	$estadoCuentaModel = new EstadoCuentaModel();
+	// 	$resp["data"]=$estadoCuentaModel->getListaPacientes($this->request->getVar('cliente'));
+	// 	return $this->respond($resp);
+	// }
+
+	public function listaPacientes()
+    {
+        $json = file_get_contents('php://input');
+        $dataEstadoCuenta = json_decode($json);
+        $estadoCuentaModel = new EstadoCuentaModel();
+        $dataEstadoCuenta = $estadoCuentaModel->getListaPacientes($dataEstadoCuenta);
+        
+        $resp["data"]=$dataEstadoCuenta;
+        // $resp["count"] =$estadoCuentaModel->getListaPacientesNums($dataEstadoCuenta)[0];
+        return $this->respond($resp);
+    }
 
 }
--- app/Models/EstadoCuentaModel.php
+++ app/Models/EstadoCuentaModel.php
@@ -6,7 +6,7 @@ use \DateInterval;
 
 class EstadoCuentaModel extends Model
 {
-  protected $table = 'cliente';
+  protected $table = 'servicio';
   protected $db;
 
   public function __construct()
@@ -27,8 +27,32 @@ public function insert_data($data = array())
     return $query->getResult();
   }
 
-  public function getListaPacientes($dato) { 
-    $query = $this->db->query('select *, CONCAT_WS(" ",nombre,a_paterno,a_materno) as nombrecompleto from servicio inner join pagoservicio on servicio.idServicio=pagoservicio.idServicio where cliente='.$dato);
+  // public function getListaPacientes($dato) { 
+  //   $query = $this->db->query('select *, CONCAT_WS(" ",nombre,a_paterno,a_materno) as nombrecompleto from servicio inner join pagoservicio on servicio.idServicio=pagoservicio.idServicio where cliente='.$dato);
+  //   return $query->getResult();
+  // }
+
+  public function getListaPacientes($estadoCuentaFiltro) {
+    $filter = ' where serv.idServicio > 0 ';
+    if(!empty($estadoCuentaFiltro)) {
+      if(!empty($estadoCuentaFiltro->fecha1) && !empty($estadoCuentaFiltro->fecha2)) {
+        $fecha1Conv = strtotime($estadoCuentaFiltro->fecha1);
+        $fecha1new = date('Y-m-d',$fecha1Conv);
+        $fecha2Conv = strtotime($estadoCuentaFiltro->fecha2);
+        $fecha2new = date('Y-m-d',$fecha2Conv);
+        $filter = $filter . ' and (serv.fechaCreacion between \'' . $fecha1new . '\' and \'' . $fecha2new .'\' ) ';
+      }
+      if(!empty($estadoCuentaFiltro->pacientes)) {
+        $filter = $filter . ' and serv.cliente= ' . $estadoCuentaFiltro->pacientes[0]->idCliente;
+      }
+  }
+
+    $query = $this->db->query('select *, CONCAT_WS(" ",serv.nombre,serv.a_paterno,serv.a_materno) as nombrecompleto from ' . $this->table . ' serv ' . ' inner join pagoservicio pag on serv.idServicio=pag.idServicio'.
+                              $filter);
+    
+    // var_dump('select *, CONCAT_WS(" ",serv.nombre,serv.a_paterno,serv.a_materno) as nombrecompleto, from ' . $this->table . ' serv ' . ' inner join pagoservicio pag on serv.idServicio=pag.idServicio'.
+    // $filter);
+
     return $query->getResult();
   }
 
